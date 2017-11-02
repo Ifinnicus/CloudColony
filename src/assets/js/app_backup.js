697cbef07cbe23f3f9cadca6277d0050745c5f98
finn <finn-troops@web.de>
2017-11-02 17:06:13 +0100

    audio-Player Bastelei

--- src/assets/js/app_backup.js
+++ src/assets/js/app_backup.js
@@ -35,110 +35,6 @@ $(document).ready(function () {
 
 
 
-
-
-
-//PLAYER 02
-
-var songs = ["Cloud-Colony-Win-a-Flight-to-Earth-II.mp3",
-    "Cloud-Colony - Our-Garden.mp3",
-    "Cloud-Colony - Psychological-Facet.mp3",
-    "Cloud-Colony - Chance.mp3",
-    "Cloud-Colony - Turbine.mp3",
-    "Cloud-Colony - Clouds.mp3",
-    "Cloud-Colony - Won.mp3",];
-
-var songTitle = document.getElementById('songTitle');
-var songSlider = document.getElementById('songSlider');
-var currentTime = document.getElementById('currentTime');
-var duration = document.getElementById('duration');
-var volumeSlider = document.getElementById('volumeSlider');
-var nextSongTitle = document.getElementById('nextSongTitle');
-
-var song = new Audio();
-var currentSong = 0;
-
-window.onload = loadSong;
-
-function loadSong() {
-    song.src = "assets/music/" + songs[currentSong];
-    songTitle.textContent = (currentSong + 1) + ". " + songs[currentSong];
-    nextSongTitle.innerHTML = "<b>Next Song: </b></br>" + (currentSong + 2) + ". " + songs[currentSong + 1 % songs.length];
-    song.playbackRate = 1;
-    song.volume = volumeSlider.value;
-    song.play();
-    setTimeout(showDuration, 1000);
-}
-
-setInterval(updateSongSlider, 1000);
-
-function updateSongSlider() {
-    var c = Math.round(song.currentTime);
-    songSlider.value = c;
-    currentTime.textContent = convertTime(c);
-    if (song.ended) {
-        next();
-    }
-}
-
-function convertTime(secs) {
-    var min = Math.floor(secs / 60);
-    var sec = secs % 60;
-    min = (min < 10) ? "0" + min : min;
-    sec = (sec < 10) ? "0" + sec : sec;
-    return (min + ":" + sec);
-}
-
-function showDuration() {
-    var d = Math.floor(song.duration);
-    songSlider.setAttribute("max", d);
-    duration.textContent = convertTime(d);
-}
-
-function playOrPauseSong(img) {
-    song.playbackRate = 1;
-    if (song.paused) {
-        song.play();
-        img.src = "assets/music/img/pauseBtn.png";
-    } else {
-        song.pause();
-        img.src = "assets/music/img/playBtn.png";
-    }
-}
-
-function next() {
-    currentSong = currentSong + 1 % songs.length;
-    loadSong();
-}
-
-function previous() {
-    currentSong--;
-    currentSong = (currentSong < 0) ? songs.length - 1 : currentSong;
-    loadSong();
-}
-
-function seekSong() {
-    song.currentTime = songSlider.value;
-    currentTime.textContent = convertTime(song.currentTime);
-}
-
-function adjustVolume() {
-    song.volume = volumeSlider.value;
-}
-
-function increasePlaybackRate() {
-    songs.playbackRate += 0.5;
-}
-
-function decreasePlaybackRate() {
-    songs.playbackRate -= 0.5;
-}
-
-
-
-
-
-
 //PLAYER 01
 
 var audio;
@@ -248,4 +144,102 @@ function showDuration(){
         }
         $('#progress').css('width',value+'%');
     })
+}
+
+
+
+//PLAYER 02
+
+var songs = ["Cloud Colony - Win a Flight to Earth II.mp3",
+    "Cloud Colony - Our Garden.mp3",
+    "Cloud Colony - Psychological Facet.mp3",
+    "Cloud Colony - Chance.mp3",
+    "Cloud Colony - Turbine.mp3",
+    "Cloud Colony - Clouds.mp3",
+    "Cloud Colony - Won.mp3",];
+
+var songTitle = document.getElementById('songTitle');
+var songSlider = document.getElementById('songSlider');
+var currentTime = document.getElementById('currentTime');
+var duration = document.getElementById('duration');
+var volumeSlider = document.getElementById('volumeSlider');
+var nextSongTitle = document.getElementById('nextSongTitle');
+
+var song = new Audio();
+var currentSong = 0;
+
+window.onload = loadSong;
+
+function loadSong() {
+    song.src = "assets/music/" + songs[currentSong];
+    songTitle.textContent = (currentSong + 1) + ". " + songs[currentSong];
+    nextSongTitle.innerHTML = "<b>Next Song: </b>" + songs[currentSong + 1 % songs.length];
+    song.playbackRate = 1;
+    song.volume = volumeSlider.value;
+    song.play();
+    setTimeout(showDuration, 1000);
+}
+
+setInterval(updateSongSlider, 1000);
+
+function updateSongSlider() {
+    var c = Math.round(song.currentTime);
+    songSlider.value = c;
+    currentTime.textContent = convertTime(c);
+    if (song.ended) {
+        next();
+    }
+}
+
+function convertTime(secs) {
+    var min = Math.floor(secs / 60);
+    var sec = secs % 60;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+    return (min + ":" + sec);
+}
+
+function showDuration() {
+    var d = Math.floor(song.duration);
+    songSlider.setAttribute("max", d);
+    duration.textContent = convertTime(d);
+}
+
+function playOrPauseSong(img) {
+    song.playbackRate = 1;
+    if (song.paused) {
+        song.play();
+        img.src = "assets/music/img/pauseBtn.png";
+    } else {
+        song.pause();
+        img.src = "assets/music/img/playBtn.png";
+    }
+}
+
+function next() {
+    currentSong = currentSong + 1 % songs.length;
+    loadSong();
+}
+
+function previous() {
+    currentSong--;
+    currentSong = (currentSong < 0) ? songs.length - 1 : currentSong;
+    loadSong();
+}
+
+function seekSong() {
+    song.currentTime = songSlider.value;
+    currentTime.textContent = convertTime(song.currentTime);
+}
+
+function adjustVolume() {
+    song.volume = volumeSlider.value;
+}
+
+function increasePlaybackRate() {
+    songs.playbackRate += 0.5;
+}
+
+function decreasePlaybackRate() {
+    songs.playbackRate -= 0.5;
 }
